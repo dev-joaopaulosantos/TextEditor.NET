0ff2fb9372f7794d6d2e377afd36c9e7a965c9d1
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-26 10:59:37 -0300

    feat: função Abrir arquivo adicionada

--- Program.cs
+++ Program.cs
@@ -37,11 +37,52 @@
 
     static void Open()
     {
+        Console.WriteLine("Qual o caminho do arquivo? ");
+        string path = Console.ReadLine();
+
+        using (var file = new StreamReader(path))
+        {
+            string text = file.ReadToEnd();
+            Console.WriteLine(text);
+        }
+
+        Console.WriteLine("");
+        Console.ReadLine();
+        Menu();
+
 
     }
 
     static void Edit()
     {
 
+        Console.WriteLine("Digite seu text abaixo. (ESC para sair)");
+        Console.WriteLine("--------------------------------------");
+        string text = "";
+
+        do
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+        }
+        while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Save(text);
+
+    }
+
+    static void Save(string text)
+    {
+        Console.WriteLine("Qual o caminho para salvar o arquivo? ");
+        var path = Console.ReadLine();
+
+        using (var file = new StreamWriter(path))
+        {
+            file.Write(text);
+        }
+
+        Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+        Console.ReadLine();
+        Menu();
     }
 }
